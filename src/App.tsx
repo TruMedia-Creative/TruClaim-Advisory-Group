9693eb3fbcd30fdd882999729824722868602559
ArryoRuma <lwtruman2@gmail.com>
2026-03-27 13:25:47 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,36 @@ import Contact from './pages/Contact';
 import Process from './pages/Process';
 import Texas from './pages/Texas';
 import Louisiana from './pages/Louisiana';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
-      <ScrollToTop />
-      <Routes>
-        <Route
-          path="/*"
-          element={
-            <Layout>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/index" element={<Home />} />
-                <Route path="/index.html" element={<Home />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/process" element={<Process />} />
-                <Route path="/texas" element={<Texas />} />
-                <Route path="/louisiana" element={<Louisiana />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-              </Routes>
-            </Layout>
-          }
-        />
-      </Routes>
-    </Router>
+    <>
+      <Router basename={import.meta.env.BASE_URL}>
+        <ScrollToTop />
+        <Routes>
+          <Route
+            path="/*"
+            element={
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/index" element={<Home />} />
+                  <Route path="/index.html" element={<Home />} />
+                  <Route path="/services" element={<Services />} />
+                  <Route path="/process" element={<Process />} />
+                  <Route path="/texas" element={<Texas />} />
+                  <Route path="/louisiana" element={<Louisiana />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+              </Layout>
+            }
+          />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
